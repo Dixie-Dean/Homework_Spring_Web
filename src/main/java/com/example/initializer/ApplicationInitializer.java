89ce53442b14b5160d18a1a677f0678e6be7e549
Dixie Dean <d.w.sokolov26@gmail.com>
2023-08-23 15:29:47 +0300

    Added initializer

---
+++ src/main/java/com/example/initializer/ApplicationInitializer.java
@@ -0,0 +1,21 @@
+package com.example.initializer;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) {
+        final var context = new AnnotationConfigWebApplicationContext();
+        context.scan("com.example");
+        context.refresh();
+
+        final var servlet = new DispatcherServlet(context);
+        final var registration = servletContext.addServlet("app", servlet);
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
+    }
+}
